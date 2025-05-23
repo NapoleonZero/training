0ff84358e8907d8adf3ae63a0f592d5f788b7d43
crybot <crybot@hotmail.it>
2025-05-23 15:17:18 +0200

    Implement transformer from scratch; still use pytorch scaled_dot_product_attention for performance reasons

--- src/models/transformer.py
+++ src/models/transformer.py
@@ -1,13 +1,15 @@
+import copy
+import math
 import torch
 from torch import nn
 from torch.jit import Final
-
+import torch.nn.functional as F
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
 from models.cnn import Conv2dBlock, DepthwiseSeparable2d
 from torchvision.ops import StochasticDepth
 from torch import Tensor
-from typing import Optional
+from typing import Optional, Union
 
 def pair(t):
     return t if isinstance(t, tuple) else (t, t)
@@ -16,6 +18,330 @@ def freeze(layer):
     for w in layer.parameters():
         w.requires_grad = False
     return layer
+
+def scaled_dot_product_attention(
+    q: Tensor,
+    k: Tensor,
+    v: Tensor,
+    attention_mask: Optional[Tensor] = None,
+    dim = None
+) -> Tensor:
+    if not dim:
+        dim = k.shape[-1]
+
+    # Batched Query-Value matrix multiplications over the last two dims:
+    # the remaining are considered as batch dimensions
+    attn = torch.matmul(q, k.transpose(-1, -2))
+
+    # Normalization: we scale by the sqrt of the dimension of each head because
+    # QK^T computes, for each head, dot products with vectors of dimension
+    # inner_dim. If the vectors were (independent and) randomly
+    # distributed with mean 0 and unit variance then the variance of the
+    # dot product would be inner_dim. So scaling by the standard
+    # deviation is a sound normalization scheme.
+    attn = attn / math.sqrt(dim)
+
+    if attention_mask is not None:
+        attn = attn + attention_mask
+
+    # Row softmax
+    attn = torch.softmax(attn, dim = -1)
+
+    # Hack to prevent softmax from producing `nan`s when entire rows of the
+    # activation matrix are "masked" with -inf. This should be better
+    # approached with MaskedTensors, but they are still a propotype
+    # feature. An alternative approach would be to use
+    # torch.finfo(attn.dtype).min as filling value instead of -inf, but this
+    # would produce a uniform distribution instead of all zeros. These
+    # values are not considered during computation due to column masking,
+    # but they might interfere during the last projections.
+
+    # NOTE: disabled because it breaks gradient flow
+    # attn = torch.nan_to_num(attn, 0.0)
+
+    # Value multiplication
+    attn = torch.matmul(attn, v)
+
+    return attn
+
+def is_initializable(module: nn.Module) -> bool:
+    return isinstance(module, tuple([nn.Linear, nn.LayerNorm]))
+
+class Replicated(nn.Module):
+    """ Wrapper module that stacks a given Module a number of times.
+        The constructor tries to reinitialize the parameters of each copied
+        layer by calling `reset_parameters()` for each child module
+        recursively.
+    """
+    def __init__(
+        self,
+        layer: nn.Module,
+        n_layers: int
+    ) -> None:
+        super().__init__()
+        layers = [copy.deepcopy(layer) for i in range(n_layers)]
+        self.stacked = nn.ModuleList(layers)
+        self.reset_parameters()
+
+    def reset_parameters(self) -> None:
+        self.apply(lambda m: m.reset_parameters() if is_initializable(m) else None)
+
+    def forward(self, x: Tensor, *args, **kwargs) -> Tensor:
+        for layer in self.stacked:
+            x = layer(x, *args, **kwargs)
+        return x
+
+
+class MultiHeadAttention(nn.Module):
+    """ Multi head attention module.
+        Query, key and value vectors share the same dimension `dim`.
+    """
+    def __init__(
+        self,
+        dim: int,
+        n_heads: int,
+        inner_dim: int = None
+    ) -> None:
+        super().__init__()
+
+        self.dim = dim
+        self.n_heads = n_heads
+        self.inner_dim = inner_dim
+
+        if not self.inner_dim:
+            self.inner_dim = dim // n_heads
+
+        # TODO: elaborate on not using biases (LayerNormalization adds biases
+        # or cancels them out by subtracting mean?)
+        self.qkv_proj = nn.Linear(dim, self.inner_dim * self.n_heads * 3, bias = False)
+        self.out_proj = nn.Linear(self.inner_dim * n_heads, dim, bias = False)
+
+    def _qkv_proj(self, query, key, value):
+        if query is key and key is value:
+            # Compute projection for query, key, value for all heads in parallel and split it.
+            qkv = self.qkv_proj(query).chunk(3, dim=-1) # tuple of 3x(B, N, DIM)
+        else:
+            # weight.T \in R^{dim \times 3 * inner_dim * n_heads}
+            d = self.inner_dim * self.n_heads
+            q_proj_weight = self.qkv_proj.weight[0:d, :]
+            k_proj_weight = self.qkv_proj.weight[d:2*d, :]
+            v_proj_weight = self.qkv_proj.weight[2*d:, :]
+
+            # No biases
+            q = F.linear(query, q_proj_weight)
+            k = F.linear(key, k_proj_weight)
+            v = F.linear(value, v_proj_weight)
+            qkv = (q, k, v)
+
+        q, k, v = map(lambda t: rearrange(t, 'b n (h d) -> b h n d', h = self.n_heads, d = self.inner_dim), qkv)
+        return q, k, v
+
+    def forward(
+        self,
+        query: Tensor,
+        key: Tensor,
+        value: Tensor,
+        causal: bool = False,
+        key_mask: Tensor = None,
+        query_mask: Tensor = None,
+    ) -> Tensor:
+        if (key_mask is None) != (query_mask is None):
+            raise ValueError("Either both key_mask and query_mask must be None, or both must be provided.")
+
+        q, k, v = self._qkv_proj(query, key, value)
+
+        # fill_value = float('-inf')
+        fill_value = torch.finfo(q.dtype).min
+        mask = None
+        if key_mask is not None:
+
+            assert key_mask.shape[1] == key.shape[1] # must match sequence length
+            assert query_mask.shape[1] == query.shape[1] # must match sequence length
+
+            assert key_mask is not None and query_mask is not None
+
+            if key_mask.dtype is not torch.bool:
+                key_mask = key_mask.bool()
+
+            if query_mask.dtype is not torch.bool:
+                query_mask = query_mask.bool()
+
+            # Add a new dimension at position 1 -> (B, 1, N)
+            key_mask = key_mask.unsqueeze(1)
+            query_mask = query_mask.unsqueeze(1)
+
+            # The transpose produces the shape -> (B, N, 1)
+            # The & operator is broadcasted along dimension 1 for the first
+            # operand and along dimension 2 for the second. This replicates the
+            # binary mask along the rows for the first operand and along the
+            # columns for the second one, which virtually creates two batches
+            # of matrices of size (B, N, N) where the second one is the
+            # transpose of the first one. By 'logically-and' them together we
+            # obtain the correct mask for each sequence in the batch
+            mask = key_mask & query_mask.transpose(1, 2)
+            mask = torch.where(~mask, fill_value, 0.0)
+
+            # Add new 'heads' dimension for broadcasting -> (B, 1, N, N)
+            # the attention matrix is (B, H, N, N) so the mask is broadcasted H
+            # times along that dimension
+            mask = mask.unsqueeze(1)
+
+        if causal:
+            # By masking the elements of the preactivation attention matrix to
+            # -inf, the softmax automatically drops them to zero while
+            # preserving the sum-to-one constraint. We can use a single
+            # attention mask for this since it's shared among every sequence
+            # (because of padding they all have the same length)
+            n = query.shape[1]
+            causal_mask = torch.full((n, n), fill_value, device=query.device).triu(diagonal=1)
+            if mask is not None:
+                mask = mask + causal_mask
+            else:
+                mask = causal_mask
+
+        # attn = scaled_dot_product_attention(q, k, v, attention_mask=mask, dim=self.inner_dim)
+        attn = F.scaled_dot_product_attention(q, k, v)
+
+        # Concatenate heads
+        attn = rearrange(attn, 'b h n d -> b n (h d)')
+
+        # Project back to dim (unnecessary if self.inner_dim * n_heads == dim)
+        if self.inner_dim * self.n_heads != self.dim:
+            attn = self.out_proj(attn)
+
+        # assert attn.shape[-1] == self.dim
+        return attn
+
+class LinformerAttention(MultiHeadAttention):
+    """ Multi head attention with linear projections on K and V
+    """
+    # TODO: rename k and sequence_length
+    def __init__(self, *args, k: int, sequence_length: int, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.proj_dim = k
+        self.max_length = sequence_length
+
+        # Using Linear so that it automatically handles initialization
+        self.E = nn.Linear(self.max_length, self.proj_dim, bias=False)
+        self.F = nn.Linear(self.max_length, self.proj_dim, bias=False)
+
+    def forward(
+        self,
+        query: Tensor,
+        key: Tensor,
+        value: Tensor,
+        causal: bool = False, # here just for compatibility reasons
+        key_mask: Tensor = None,
+        query_mask: Tensor = None,
+        full = False,
+    ) -> Tensor:
+        if (key_mask is None) != (query_mask is None):
+            raise ValueError('Either both key_mask and query_mask must be None, or both must be provided.')
+
+        if causal:
+            raise ValueError('Warning: causal masking is not supported by the Linformer attention')
+
+        q, k, v = self._qkv_proj(query, key, value)
+
+        # TODO: mask before projecting on (query, key, value)
+        if query_mask is not None:
+            q = q.masked_fill(~query_mask.unsqueeze(-1).unsqueeze(1).bool(), 0.0)
+
+        # Share same mask for K and V
+        if key_mask is not None:
+            k = k.masked_fill(~key_mask.unsqueeze(-1).unsqueeze(1).bool(), 0.0)
+            v = v.masked_fill(~key_mask.unsqueeze(-1).unsqueeze(1).bool(), 0.0)
+
+        # Broadcast E @ K and F @ V over batch and head dimensions
+        if not full:
+            proj_k = self.E.weight
+            proj_v = self.F.weight
+
+            if key.shape[1] < self.max_length:
+                proj_k = proj_k[:, :key.shape[1]]
+                proj_v = proj_v[:, :key.shape[1]]
+
+            k = torch.matmul(proj_k, k)
+            v = torch.matmul(proj_v, v)
+
+        # attn = scaled_dot_product_attention(q, k, v, dim = self.inner_dim)
+        attn = F.scaled_dot_product_attention(q, k, v)
+
+        # Concatenate heads
+        attn = rearrange(attn, 'b h n d -> b n (h d)')
+
+        # Project back to dim (unnecessary if self.inner_dim * n_heads == dim)
+        if self.inner_dim * self.n_heads != self.dim:
+            attn = self.out_proj(attn)
+
+        assert attn.shape[-1] == self.dim
+        return attn
+
+class Transformer(nn.Module):
+    def __init__(self, encoder: nn.Module, decoder: Optional[nn.Module] = None):
+        """ TODO """
+        super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.dim = self.encoder.dim
+
+    def forward(
+        self,
+        enc_in: Tensor,
+        dec_in: Optional[Tensor] = None,
+        enc_mask: Optional[Tensor] = None,
+        dec_mask: Optional[Tensor] = None,
+        return_enc_output: bool = False,
+    ):
+        enc_out = self.encoder(enc_in, mask = enc_mask)
+
+        if self.decoder is not None:
+            dec_out = self.decoder(dec_in, enc_out, dec_mask = dec_mask, enc_mask = enc_mask)
+
+            if return_enc_output:
+                return enc_out, dec_out
+            else:
+                return dec_out
+        return enc_out
+
+    def reset_parameters(self) -> None:
+        self.encoder.reset_parameters()
+        if self.decoder is not None:
+            self.decoder.reset_parameters()
+
+# TODO: dropout, gelu, 
+class TransformerEncoder(Replicated):
+    def __init__(self,*args, causal: bool = False, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.causal = causal
+        self.dim = self.stacked[0].dim
+
+    def forward(self, x: Tensor, mask: Optional[Tensor] = None) -> Tensor:
+        return super().forward(x, causal = self.causal, mask = mask)
+
+class TransformerEncoderLayer(nn.Module):
+    def __init__(self, dim: int, mlp_dim: int, attention: nn.Module):
+        super().__init__()
+        self.dim = dim
+        self.mlp_dim = mlp_dim
+
+        self.attention = attention
+        if not self.attention:
+            self.attention = MultiHeadAttention(dim, n_heads = 8, inner_dim = dim // 8)
+
+        self.norm1 = nn.LayerNorm(dim)
+        self.norm2 = nn.LayerNorm(dim)
+
+        self.mlp = nn.Sequential(
+            nn.Linear(dim, mlp_dim, bias = False),
+            nn.ReLU(),
+            nn.Linear(mlp_dim, dim, bias = False)
+        )
+
+    def forward(self, x, causal = False, mask = None):
+        x = self.norm1(self.attention(x, x, x, causal = causal, query_mask = mask, key_mask = mask) + x) # Norm first = False (original paper)
+        x = self.norm2(self.mlp(x) + x) # Norm first = False (original paper)
+        return x
 
 class SDTransformerEncoder(nn.TransformerEncoder):
     """ Implements a Transformer encoder architecture with batch or row
@@ -171,19 +497,25 @@ class ViT(nn.Module):
         self.pos_embedding = nn.Parameter(torch.randn(1, num_patches + 1, dim))
         self.dropout = nn.Dropout(emb_dropout)
 
-        self.transformer = nn.TransformerEncoder(
-                nn.TransformerEncoderLayer(
-                    d_model=dim,
-                    nhead=heads,
-                    dim_feedforward=mlp_dim,
-                    dropout=dropout,
-                    activation='gelu',
-                    norm_first=True,
-                    batch_first=True
-                    ),
-                num_layers=stages_depth[0] if hierarchical else depth,
-                norm=nn.LayerNorm(dim)
-                )
+        attn = MultiHeadAttention(dim=dim, n_heads=heads)
+        self.transformer = TransformerEncoder(
+            TransformerEncoderLayer(dim, mlp_dim, attn),
+            n_layers=stages_depth[0] if hierarchical else depth
+        )
+                                                                      
+        # self.transformer = nn.TransformerEncoder(
+        #         nn.TransformerEncoderLayer(
+        #             d_model=dim,
+        #             nhead=heads,
+        #             dim_feedforward=mlp_dim,
+        #             dropout=dropout,
+        #             activation='gelu',
+        #             norm_first=True,
+        #             batch_first=True
+        #             ),
+        #         num_layers=stages_depth[0] if hierarchical else depth,
+        #         norm=nn.LayerNorm(dim)
+        #         )
 
         out_dim=dim
         merging_layers = []
@@ -191,24 +523,19 @@ class ViT(nn.Module):
             for l in range(merging_blocks):
                 in_dim = out_dim
                 out_dim *= 2
+                attn = MultiHeadAttention(dim=out_dim, n_heads=heads)
                 k = stages_depth[l + 1]
                 merge = nn.Sequential(
-                        PatchMerging(patch_size*(2**(l+1)), in_dim, out_dim, mode=merging_strategy),
-                        nn.TransformerEncoder(
-                            nn.TransformerEncoderLayer(
-                                d_model=out_dim,
-                                nhead=heads,
-                                # dim_feedforward=(out_dim * 4),
-                                dim_feedforward=mlp_dim,
-                                dropout=dropout,
-                                activation='gelu',
-                                norm_first=True,
-                                batch_first=True
-                                ),
-                            num_layers=k,
-                            norm=nn.LayerNorm(out_dim)
-                            )
-                        )
+                    PatchMerging(patch_size*(2**(l+1)), in_dim, out_dim, mode=merging_strategy),
+                    TransformerEncoder(
+                        TransformerEncoderLayer(
+                            out_dim,
+                            mlp_dim, # dim_feedforward=(out_dim * 4),
+                            attn),
+                        n_layers=k
+                        # norm=nn.LayerNorm(out_dim)
+                    )
+                )
                 merging_layers.append(merge)
 
         self.stages_stack = nn.ModuleList(merging_layers)
@@ -221,10 +548,10 @@ class ViT(nn.Module):
         x = self.to_patch_embedding(img)
         b, n, _ = x.shape
 
-        aux = self.to_aux_embedding(aux)
-        aux = aux[:, None, :] # Add dummy (n) dimension
+        aux = self.to_aux_embedding(aux) # Bx3 -> BxD
+        aux = aux[:, None, :] # Add dummy (n) dimension: BxD -> BxNxD
 
-        x = torch.cat((aux, x), dim=1)
+        x = torch.cat((aux, x), dim=1) # Add aux as an additional token patch
 
         # TODO: make ViT pos embedding optional
         x += self.pos_embedding[:, :(n + 1)]
